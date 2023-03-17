14568fc3af3ed6d276a27a8b8addabdf642252f4
zunayedlimon <zahmedlimon@gmail.com>
2023-03-18 01:22:29 +0600

    added

---
+++ index.js
@@ -0,0 +1,21 @@
+// How to set an item to session storage
+sessionStorage.setItem('myKey', 'myValue');
+
+// How to get an item from session storage
+const getItem = sessionStorage.getItem('myKey');
+console.log(getItem);
+
+// How to remove an item from session storage
+sessionStorage.removeItem('myKey'); //after coding this previous value removed 
+
+// How to clear all item on session storage
+sessionStorage.setItem('roll-1', 25);
+sessionStorage.setItem('roll-2', 26);
+sessionStorage.setItem('roll-3', 27);
+sessionStorage.setItem('roll-4', 28);
+sessionStorage.setItem('roll-5', 29);
+
+sessionStorage.clear(); //after this all date cleared
+
+
+
